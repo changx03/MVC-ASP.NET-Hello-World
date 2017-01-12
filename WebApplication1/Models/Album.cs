db26b36bb202661b396a80ec311f18428020dd87
changx03 <luke.x.chang@gmail.com>
2017-01-12 23:28:27 +1300

    add model

---
+++ WebApplication1/Models/Album.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Models
+{
+    public class Album
+    {
+        public int AlbumId { get; set; }
+
+        public string Title { get; set; }
+
+        public Artest Artist { get; set; }
+    }
+}
---
+++ WebApplication1/Models/Artest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Artest
+    {
+        public int ArtistId { get; set; }
+
+        public string Name { get; set; }
+
+        public List<Album> Albums { get; set; }
+
+    }
+}
